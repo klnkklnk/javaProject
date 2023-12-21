010938d214e74f23663aabe8a3336b0f82b26995
Артем Калиниченко <kalinichenko.artyom@gmail.com>
2023-12-21 22:42:12 +0200

    added password validation in HashService added verifyUser function in DbService

--- src/main/java/com/javafxapp/database/DbService.java
+++ src/main/java/com/javafxapp/database/DbService.java
@@ -1,11 +1,11 @@
 package com.javafxapp.database;
 
+import com.javafxapp.models.HashService;
 import com.javafxapp.models.User;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.*;
 
 public class DbService
 {
@@ -19,20 +19,41 @@ public class DbService
 	public void addUserToDb(User user) throws Exception, SQLException
 	{
 		String login = user.getLogin();
-		String password = user.getPassword();
+		String password = HashService.generatePasswordHash(user.getPassword());
+		
 		if (login.isEmpty() || password.isEmpty())
 		{
 			throw new Exception("login or password can not be empty");
 		}
 		try
 		{
-			String query = "INSERT INTO user(login, password) VALUES ('" + login + "', '" + password + "')";
-			Statement statement = this.connection.createStatement();
-			statement.executeUpdate(query);
+			String query = "INSERT INTO user (login, password) VALUES (?, ?)";
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, login);
+			statement.setString(2, password);
+			statement.executeUpdate();
 		}
 		catch (SQLException sqlException)
 		{
 			throw new SQLException("Couldn't add a user: " + sqlException);
 		}
 	}
+	
+	public boolean verifyUser(User user)
+	{
+		try
+		{
+			String query = "SELECT password FROM user WHERE login=?";
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, user.getLogin());
+			ResultSet resultSet = statement.executeQuery();
+			resultSet.next();
+			return HashService.validatePassword(user.getPassword(), resultSet.getString("password"));
+ 		}
+		catch (SQLException | InvalidKeySpecException | NoSuchAlgorithmException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
 }
+
